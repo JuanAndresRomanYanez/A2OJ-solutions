cf0ce420f7e14d44bf727e1bce8083514ba90108
Juan Andrés Román Yáñez <andy_blest@hotmail.com>
2017-09-25 00:10:53 -0400

    Create  1. Omar.cpp

---
+++ 1. Omar.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+int main()
+{
+optimizar_io;
+   int a,b,c;
+   cin>>c;
+   for(int i=0;i<c;i++)
+   {
+       cin>>a>>b;
+       cout<<a+b<<"\n";
+   }
+}
